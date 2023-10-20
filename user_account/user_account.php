6c0ef7acb48f46e7cf3dd98a2f3185e658eddc02
Sanket <sanketgupta1000@gmail.com>
2023-10-20 17:34:37 +0530

    Made signup-login frontend ready for feedback messages from backend

--- user_account/user_account.php
+++ user_account/user_account.php
@@ -35,15 +35,32 @@
                         
                         <!-- tab list -->
                         <ul class="nav nav-tabs nav-justified mb-4" id="accountTabs" role="tablist">
-
                             <li class="nav-item" role="presentation">
                                 <a
-                                class="nav-link<?php if(isset($_GET["signup"])) echo " active"; ?>"
+                                class="nav-link
+                                    <?php
+                                        //if the page is accessed with 'signup' in get parameter, signup button will be active
+                                        if(isset($_GET["signup"]))
+                                        {
+                                            echo " active";
+                                        }
+                                    ?>"
                                 id="signupTab"
                                 data-mdb-toggle="tab"
                                 href="#signupPane"
                                 aria-controls="signup"
-                                aria-selected="<?php if(isset($_GET["signup"])) echo "true"; else echo "false"; ?>"
+                                aria-selected="
+                                    <?php
+                                        if(isset($_GET["signup"]))
+                                        {
+                                            //if the page is accessed with 'signup' in get parameter, signup button will be active
+                                            echo "true";
+                                        }
+                                        else
+                                        {
+                                            echo "false";
+                                        }
+                                    ?>"
                                 role="tab"
                                 >
                                     Sign Up
@@ -52,12 +69,30 @@
 
                             <li class="nav-item" role="presentation">
                                 <a
-                                class="nav-link<?php if(isset($_GET["login"])) echo " active"; ?>"
+                                class="nav-link
+                                    <?php
+                                        //if the page is accessed with 'login' in get parameter, login button will be active
+                                        if(isset($_GET["login"]))
+                                        {
+                                        echo " active";
+                                        }
+                                    ?>"
                                 id="loginTab"
                                 data-mdb-toggle="tab"
                                 href="#loginPane"
                                 aria-controls="login"
-                                aria-selected="<?php if(isset($_GET["login"])) echo "true"; else echo "false"; ?>"
+                                aria-selected="
+                                    <?php
+                                        if(isset($_GET["login"]))
+                                        {
+                                            //if the page is accessed with 'login' in get parameter, login button will be active
+                                            echo "true";
+                                        }
+                                        else
+                                        {
+                                            echo "false";
+                                        }
+                                    ?>"
                                 role="tab"
                                 >
                                     Login
@@ -71,7 +106,14 @@
 
                             <!-- sign up panel -->
                             <form
-                            class="tab-pane fade<?php if(isset($_GET["signup"])) echo " show active"; ?>"
+                            class="tab-pane fade
+                                <?php
+                                    //if the page is accessed with 'signup' in get parameter, signup pane will be active
+                                    if(isset($_GET["signup"]))
+                                    {
+                                        echo " show active";
+                                    }
+                                ?>"
                             id="signupPane"
                             role="tabpanel"
                             aria-labelledby="signupTab"
@@ -80,9 +122,36 @@
 
                                 <div class="row gy-3 gy-md-4">
 
+                                    <?php
+                                        //feedback from backend for email already taken
+                                        if(isset($_GET["emailTaken"]))
+                                        {
+                                            echo '<div class="col-12">
+                                                    <div class="alert alert-danger mb-0 p-3 text-center">
+                                                        Email is already registered. Try logging in
+                                                    </div>
+                                                </div>';
+                                        }
+                                    ?>
+
                                     <div class="col-12">
                                         <div class="form-outline">
-                                            <input type="email" id="signupEmail" name="email" class="form-control" required>
+                                            <input type="email" id="signupEmail" name="email" required class="form-control
+                                                                                                            <?php
+                                                                                                                //validation from back-end
+                                                                                                                if(isset($_GET["invalidSignupEmail"]))
+                                                                                                                {
+                                                                                                                    echo " is-invalid";
+                                                                                                                }
+                                                                                                            ?>"
+                                                                                                            <?php
+                                                                                                                //validation from backend, to show email after backend found wrongly filled signup form
+                                                                                                                if(isset($_GET["signupEmail"]))
+                                                                                                                {
+                                                                                                                    echo ' value="'.$_GET['signupEmail'].'"';
+                                                                                                                }                                                                                                
+                                                                                                            ?>
+                                                                                                        >
                                             <label for="signupEmail" class="form-label">Email</label>
                                             <div class="invalid-feedback" id="invalid-signupEmail">Please enter valid email</div>
                                         </div>
@@ -90,7 +159,15 @@
 
                                     <div class="col-12">
                                         <div class="form-outline">
-                                            <input type="password" id="signupPass" name="password" class="form-control" required>
+                                            <input type="password" id="signupPass" name="password" required class="form-control
+                                                                                                                <?php
+                                                                                                                    //validation from back-end
+                                                                                                                    if(isset($_GET["emptySignupPass"]))
+                                                                                                                    {
+                                                                                                                        echo " is-invalid";
+                                                                                                                    }
+                                                                                                                ?>
+                                                                                                            ">
                                             <label for="signupPass" class="form-label">Password</label>
                                             <div class="invalid-feedback" id="empty-signupPass">Please enter password</div>
                                         </div>
@@ -98,9 +175,17 @@
 
                                     <div class="col-12">
                                         <div class="form-outline">
-                                            <input type="password" id="signupConfirmPass" name="confirmpassword" class="form-control" required>
+                                            <input type="password" id="signupConfirmPass" name="confirmpassword" required class="form-control
+                                                                                                                                <?php
+                                                                                                                                    //validation from back-end
+                                                                                                                                    if(isset($_GET["invalidSignupConfirmPass"]))
+                                                                                                                                    {
+                                                                                                                                        echo " is-invalid";
+                                                                                                                                    }
+                                                                                                                                ?>
+                                                                                                                            ">
                                             <label for="signupConfirmPass" class="form-label">Confirm Password</label>
-                                            <div class="invalid-feedback" id="empty-signupConfirmPass">Please enter valid confirm password</div>
+                                            <div class="invalid-feedback" id="invalid-signupConfirmPass">Please enter valid confirm password</div>
                                         </div>
                                     </div>
 
@@ -114,7 +199,14 @@
 
                             <!-- login panel -->
                             <form
-                            class="tab-pane fade<?php if(isset($_GET["login"])) echo " show active"; ?>"
+                            class="tab-pane fade
+                                <?php
+                                    //if the page is accessed with 'login' in get parameter, login pane will be active
+                                    if(isset($_GET["login"]))
+                                    {
+                                        echo " show active";
+                                    }
+                                ?>"
                             id="loginPane"
                             role="tabpanel"
                             aria-labelledby="loginTab"
@@ -123,9 +215,36 @@
 
                                 <div class="row gy-3 gy-md-4">
 
+                                    <?php
+                                        //feedback from backend for incorrect credentials
+                                        if(isset($_GET["incorrect"]))
+                                        {
+                                            echo '<div class="col-12">
+                                                    <div class="alert alert-danger mb-0 p-3 text-center">
+                                                        Incorrect details. Please enter correct details
+                                                    </div>
+                                                </div>';
+                                        }
+                                    ?>
+
                                     <div class="col-12">
                                         <div class="form-outline">
-                                            <input type="email" id="loginEmail" name="email" class="form-control" required>
+                                            <input type="email" id="loginEmail" name="email" required class="form-control
+                                                                                                            <?php
+                                                                                                                //validation from back-end
+                                                                                                                if(isset($_GET["invalidLoginEmail"]))
+                                                                                                                {
+                                                                                                                    echo " is-invalid";
+                                                                                                                }
+                                                                                                            ?>"
+                                                                                                            <?php
+                                                                                                                //validation from backend, to show email after backend found wrongly filled login form
+                                                                                                                if(isset($_GET["loginEmail"]))
+                                                                                                                {
+                                                                                                                    echo ' value="'.$_GET['loginEmail'].'"';
+                                                                                                                }                                                                                                
+                                                                                                            ?>
+                                                                                                        >
                                             <label for="loginEmail" class="form-label">Email</label>
                                             <div class="invalid-feedback" id="invalid-loginEmail">Please enter valid email</div>
                                         </div>
@@ -133,7 +252,15 @@
 
                                     <div class="col-12">
                                         <div class="form-outline">
-                                            <input type="password" id="loginPass" name="password" class="form-control" required>
+                                            <input type="password" id="loginPass" name="password" required class="form-control
+                                                                                                            <?php
+                                                                                                                //validation from backend
+                                                                                                                if(isset($_GET["emptyLoginPass"]))
+                                                                                                                {
+                                                                                                                    echo " is-invalid";
+                                                                                                                }
+                                                                                                            ?>"
+                                                                                                        >
                                             <label for="loginPass" class="form-label">Password</label>
                                             <div class="invalid-feedback" id="empty-loginPass">Please enter password</div>
                                         </div>
