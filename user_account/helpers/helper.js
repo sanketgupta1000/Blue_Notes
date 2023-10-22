ae0296f7f9d450735300a6ea8090851e094bdb81
Sanket <sanketgupta1000@gmail.com>
2023-10-22 14:26:55 +0530

    Updated helper.js

--- user_account/helpers/helper.js
+++ user_account/helpers/helper.js
@@ -1,21 +1,35 @@
 //will contain utility functions, such as displaying error/success messages
 
+//error/success messages:
+const badResponse = "Bad response from server. Please try again later";
+const badRequest = "Bad request to server. Please try again later";
+const badDatabaseConn = "Bad database connection. Please try again later";
+const mailServiceDown = "Mail service is down. Please try again later";
+const emailTaken = "Email is already registered. Try logging in";
+
 let accountTabs = document.getElementById("accountTabs");
 
 //function to remove any error/success message already present
-function removeMessage()
+function removeMessages()
 {
     if((accountTabs.nextElementSibling.id=="success")||(accountTabs.nextElementSibling.id=="error"))
     {
         accountTabs.nextElementSibling.remove();
     }
+
+    //also removing any feedback on inputs
+    signupEmail.classList.remove("is-valid", "is-invalid");
+    signupPass.classList.remove("is-valid", "is-invalid");
+    signupConfirmPass.classList.remove("is-valid", "is-invalid");
+    loginEmail.classList.remove("is-valid", "is-invalid");
+    loginPass.classList.remove("is-valid", "is-invalid");
 }
 
 //function to show error
 function displayError(errstr)
 {
     //will remove any error/success message already present
-    removeMessage();
+    removeMessages();
 
     let error = document.createElement("div");
     error.id="error";
@@ -29,7 +43,7 @@ function displayError(errstr)
 //function to show success
 function displaySuccess(successstr)
 {
-    removeMessage();
+    removeMessages();
 
     let success = document.createElement("div");
     success.id = "success";
@@ -37,4 +51,15 @@ function displaySuccess(successstr)
     success.textContent = successstr;
 
     accountTabs.insertAdjacentElement("afterend", success);
+}
+
+//function to hash the password on client side
+async function hashPassword(password)
+{
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, "0")).join("");
+    return hashHex;
 }
